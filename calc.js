919ee99ed70fb8f41c8de6ae66dc5b2ba5ce7ce2
Cinnabonmon <Cinnabonmon1@gmail.com>
2025-12-02 14:10:34 -0600

    Changed comments in Javascript file and added decimal point to allowed characters

--- calc.js
+++ calc.js
@@ -1,10 +1,10 @@
 const display = document.getElementById("display");
 
-// Adds the number you click to the display
+// Add the numbers on the display
 function appendToDisplay(input) {
 	display.value += input;
 }
-// Clears the display screen and resets it back to empty
+// Clear the display screen and reset it back to empty
 function clearDisplay() {
 	display.value = " ";
 }
@@ -18,8 +18,8 @@ function sum() {
 }
 // Key events for all numbers and operators for keyboard use.
 document.addEventListener("keydown", (input) => {
-	// Only allowed keys in this string expression
-	const allowedNum = /^[0-9+\-*/()]$/;
+	// Only allowed keys 
+	const allowedNum = /^[0-9+\-*/().]$/;
 	// Special keys: Clear, Enter, Backspace
 	switch (input.key) {
 		case "Delete":
@@ -34,7 +34,7 @@ document.addEventListener("keydown", (input) => {
 		default:
 			input.preventDefault();
 	}
-	// Tests the keys that are pressed to make sure its only the ones stored in the string expression in the allowedNum variable
+	// Test the keys that are pressed to make sure its only the ones stored in the string expression in the allowedNum variable
 	if (input.key.length === 1 && allowedNum.test(input.key)) {
 		display.value += input.key;
 	}
